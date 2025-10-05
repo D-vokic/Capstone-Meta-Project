dd2fb3df040e52819a1bd157ad8139dbcf4cf854
Dusko Vokic <vokic.dusko@gmail.com>
2025-10-05 22:49:22 +0200

    Add modern ReservationsPage layout with background image and update routes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,66 @@
+// import "./App.css";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Navbar from "./components/Navbar.js";
+// import Hero from "./components/Hero.js";
+// import Main from "./components/Main.js";
+// import Testimonials from "./components/Testimonials.js";
+// import About from "./components/About.js";
+// import Footer from "./components/Footer.js";
+// import AboutPage from "./pages/AboutPage.js";
+// import MenuPage from "./pages/MenuPage.js";
+// import Reservations from "./pages/ReservationsPage.js";
+
+// function App() {
+//   return (
+//     <Router>
+//       <Navbar />
+//       <Routes>
+//         <Route
+//           path="/"
+//           element={
+//             <>
+//               <Hero />
+//               <Main />
+//               <Testimonials />
+//               <About />
+//               <Footer />
+//             </>
+//           }
+//         />
+//         <Route
+//           path="/about"
+//           element={
+//             <>
+//               <AboutPage />
+//               <Footer />
+//             </>
+//           }
+//         />
+//         <Route
+//           path="/menu"
+//           element={
+//             <>
+//               <MenuPage />
+//               <Footer />
+//             </>
+//           }
+//         />
+//         <Route
+//           path="/reservations"
+//           element={
+//             <>
+//               <Reservations />
+//               <Footer />
+//             </>
+//           }
+//         />
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar.js";
@@ -8,7 +71,7 @@ import About from "./components/About.js";
 import Footer from "./components/Footer.js";
 import AboutPage from "./pages/AboutPage.js";
 import MenuPage from "./pages/MenuPage.js";
-import Reservations from "./pages/ReservationsPage.js";
+import ReservationsPage from "./pages/ReservationsPage.js";
 
 function App() {
   return (
@@ -49,7 +112,7 @@ function App() {
           path="/reservations"
           element={
             <>
-              <Reservations />
+              <ReservationsPage />
               <Footer />
             </>
           }
